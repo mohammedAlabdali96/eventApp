78e908337f5625df7ffef97c3d4a965664eb0280
mo <mohammed@artlimes.com>
2020-11-30 11:40:19 +0200

    disply the posts

--- App/server/index.js
+++ App/server/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 
-const CONNECTION_URL="mongodb+srv://mohammedAlabdali:07712326526Al@cluster0.r7k0q.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const CONNECTION_URL="mongodb+srv://Mohammed_1996:07712326526Al@cluster0.zvtx6.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
 .then(() => app.listen(PORT, ()=>console.log(`server running ${PORT}`)))
